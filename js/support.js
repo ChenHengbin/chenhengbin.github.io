41c6e51f397095cf15d845ffc7252f1add078bd6
ChenHengbin <812682293@qq.com>
2014-04-16 23:44:37 +0800

    d
    d

--- js/support.js
+++ js/support.js
@@ -2,11 +2,3 @@
 	this.URL = "http://www.simplewish.cn/";
 	this.bookMarkUrl = this.URL+"data/BookMark.json";
 }
-
-function getBookMarkData(){
-	$.getJSON(constant.bookMarkUrl,function(data){
-	   var array = data.CDN;
-	   console.log(array[0].url);
-	});
-}
-
