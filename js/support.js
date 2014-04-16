241116fc0c8514b02aae7549e792df3969ca078b
ChenHengbin <812682293@qq.com>
2014-04-16 23:54:18 +0800

    d
    d

--- js/support.js
+++ js/support.js
@@ -1,4 +1,13 @@
-﻿var constant = function Constant(){
+﻿function Constant(){
 	this.URL = "http://www.simplewish.cn/";
 	this.bookMarkUrl = this.URL+"data/BookMark.json";
+}
+
+var constant = new Constant();
+
+function getBookMarkData(){
+	$.getJSON(constant.bookMarkUrl,function(data){
+	   var array = data.CDN;
+	   console.log(array[0].url);
+	});
 }
