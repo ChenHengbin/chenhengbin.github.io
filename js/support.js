051edf0d9c08479165d9a7bcc39fe3ee97745e61
ChenHengbin <812682293@qq.com>
2014-04-16 23:32:50 +0800

    change
    change

--- js/support.js
+++ js/support.js
@@ -1,4 +1,13 @@
-function Constant(){
+var constant = function Constant(){
 	this.URL = "http://www.simplewish.cn/";
 	this.bookMarkUrl = this.URL+"data/BookMark.json";
 }
+
+//获取书签数据
+function getBookMarkData(){
+	$.getJSON(constant.bookMarkUrl,function(data){
+	   var array = data.CDN;
+	   console.log(array[0].url);
+	});
+}
+
