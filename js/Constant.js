a68d3c37c8dfa237a22ceb7e796f1c77f233c975
ChenHengbin <812682293@qq.com>
2014-04-16 23:41:32 +0800

    chagne
    chagne

--- js/Constant.js
+++
@@ -1,4 +0,0 @@
-function Constant = function(){
-	this.URL = "http://www.simplewish.cn";
-	this.bookMarkUrl = "/data/BookMark.json";
-}
--- js/support.js
+++ js/support.js
@@ -1,9 +1,8 @@
-var constant = function Constant(){
+﻿var constant = function Constant(){
 	this.URL = "http://www.simplewish.cn/";
 	this.bookMarkUrl = this.URL+"data/BookMark.json";
 }
 
-//获取书签数据
 function getBookMarkData(){
 	$.getJSON(constant.bookMarkUrl,function(data){
 	   var array = data.CDN;
