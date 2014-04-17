042a108c45030811446f81c2c9e7e426cb96a617
ChenHengbin <812682293@qq.com>
2014-04-17 23:14:03 +0800

    d
    d

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,17 @@
 function BmarkCntl($scope,$http){
-	$scope.bookMarks = null;
+    $http.get(swModule.constant.BOOK_MARK_URL).success(function(data, status, headers, config) {
+		$scope.bookMarks = data;
+	});
+}
 
+function DreamContl($scope,$http){
+	$http.get(swModule.constant.DREAM_URL).success(function(data, status, headers, config) {
+		$scope.dreams = data;
+	});
+}
 
-        $http.get(swModule.constant.BOOK_MARK_URL).success(function(data, status, headers, config) {
-			$scope.bookMarks = data;
-		});
-
+function ScheContl($scope,$http){
+	$http.get(swModule.constant.SCHE_URL).success(function(data, status, headers, config) {
+		$scope.dreams = data;
+	});	
 }
--- js/support.js
+++ js/support.js
@@ -1,6 +1,8 @@
 ﻿function Constant(){
 	this.SERVER_URL = "http://www.simplewish.cn/";
 	this.BOOK_MARK_URL = this.SERVER_URL+"data/BookMark.json";
+	this.DREAM_URL = this.SERVER_URL+"data/Dream.json";
+	this.SCHE_URL = this.SERVER_URL+"data/Schedule.json";
 }
 
 function NavBar(){
