1e17794bcfe33c74d476947714c66284a7250160
ChenHengbin <812682293@qq.com>
2014-04-21 13:28:30 +0800

    d
    d

--- js/support.js
+++ js/support.js
@@ -5,41 +5,6 @@
 	this.SCHE_URL = this.SERVER_URL+"data/Schedule.json";
 }
 
-function NavBar(){
-	this.bmark = document.getElementById("bmark");
-	this.dream = document.getElementById("dream");
-	this.sche = document.getElementById("sche");
-	this.bmark.onclick = function(){
-		document.getElementById("getBmarkBtn").click();
-		switchActive("bmark");
-	}
-
-	this.dream.onclick = function(){
-		document.getElementById("getDreamBtn").click();
-		switchActive("dream");
-	}
-
-	this.sche.onclick = function(){
-		document.getElementById("getScheBtn").click();
-		switchActive("sche");
- 	}
-
- 	//点击导航栏按钮后，改变导航栏的样式和网页中的内容
- 	function switchActive(id){
- 		var ul = document.querySelector(".navbar-nav");
- 		var childList = ul.childNodes;
- 		for (var i = childList.length - 1; i >= 0; i--) {
- 			childList[i].className="";
- 		};
- 		document.getElementById(id).className="active";
- 		var contents = document.getElementsByClassName("content");
- 		for (var i = contents.length - 1; i >= 0; i--) {
- 			contents[i].style.display = "none";
- 		};
- 		document.getElementById(id+"-content").style.display = "";
- 	}
- }
-
 /*判断是否为手持设备*/
 function IsPC() 
 { 
@@ -66,10 +31,13 @@ window.onload = function(){
 	swModule.navBar.bmark.click();
 
 	document.onclick = function(event){
-		var eve = window.event || event ; 
-		var ele = eve['srcElement']||eve['target'];
-		if(ele.id!='toggleBtn'){
-			document.getElementById('toggleBtn').click();
+		var toggleBtn = document.getElementById("toggleBtn");
+		if(toggleBtn.style.display!="none"){
+			var eve = window.event || event ; 
+			var ele = eve['srcElement']||eve['target'];	
+			if(ele.id!='toggleBtn'){
+				toggleBtn.click();
+			}
 		}
 	}
 }
