6b1f28372903a9dc5be9844f5040426f9888848a
ChenHengbin <812682293@qq.com>
2014-04-22 10:49:48 +0800

    d
    d

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,27 @@
-//控制器
-function BmarkCntl($scope,$http){
-	$scope.getBmarkList = function(){
-		$http.get(swModule.constant.BOOK_MARK_URL).success(function(data, status, headers, config) {
-		$scope.bookMarks = data;
-		});	
-	}
-}
+//controllers
+swModule.controller('BmarkCntl',
+	function BmarkCntl($scope,$http){
+		$scope.getBmarkList = function(){
+			$http.get(swModule.constant.BOOK_MARK_URL).success(function(data, status, headers, config) {
+				$scope.bookMarks = data;
+			});	
+		}
+	}).controller('DreamContl',
+	function DreamContl($scope,$http){
+		$scope.getDreamList = function(){
+			$http.get(swModule.constant.DREAM_URL).success(function(data, status, headers, config) {
+				$scope.dreams = data;
+			});	
+		}
+	}).controller('ScheContl',
+	function ScheContl($scope,$http){
+		$scope.getScheList = function(){
+			$http.get(swModule.constant.SCHE_URL).success(function(data, status, headers, config) {
+				$scope.sches = data;
+			});		
+		}
+	});
 
-function DreamContl($scope,$http){
-	$scope.getDreamList = function(){
-		$http.get(swModule.constant.DREAM_URL).success(function(data, status, headers, config) {
-			$scope.dreams = data;
-		});	
-	}
-}
 
-function ScheContl($scope,$http){
-	$scope.getScheList = function(){
-		$http.get(swModule.constant.SCHE_URL).success(function(data, status, headers, config) {
-			$scope.sches = data;
-		});		
-	}
-}
+
+
