41ac5532effc6957acc5efe9dec6783cedfcd533
ChenHengbin <812682293@qq.com>
2014-05-03 18:08:35 +0800

    d
    d

--- js/auto_play.js
+++ js/auto_play.js
@@ -1,7 +1,16 @@
-setInterval(function(){
-	console.log('还剩'+minutes+'分'+seconds+'秒');
-	if($('#string').html()=='此课件观看时长已满足！'){
-		window.close();
+function run(){
+	if(typeof(minutes)!='undefined'&typeof(seconds)!='undefined'){
+		console.log('还剩'+minutes+'分'+seconds+'秒');
+	}
+	if(document.getElementById("string").innerHTML=='此课件观看时长已满足！'){
+		console.log('--------');
+		thisPlayer.pause();//暂停播放
+		if(task.length>0){
+			setTimeout(function(){
+				window.location=document.getElementById('nextUrl').value;
+			},180000);	
+		}
+		clearInterval(auto_play);//停止轮询
 	}
 	if(thisPlayer.getState()=='PAUSED'&&document.getElementById('RecordBut').disabled==true||
 	   thisPlayer.getState()=='IDLE'&&document.getElementById('RecordBut').disabled==true){
@@ -11,4 +20,6 @@ setInterval(function(){
 		document.getElementById('RecordBut').click();
 		document.getElementById('RecordBut').disabled = 'true';
 	}
-},30000);
+}
+
+auto_play = setInterval('run()',30000);
