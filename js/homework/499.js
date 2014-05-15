8f6cb1ed35c95b0e42e7d70586b68cba20951f9f
ChenHengbin <812682293@qq.com>
2014-05-15 14:50:38 +0800

    d
    d

---
+++ js/homework/499.js
@@ -0,0 +1,60 @@
+﻿//一、1
+document.getElementsByName('Ans834')[0].value='学习文学作品';
+document.getElementsByName('Ans834')[1].value='理解体验作品';
+document.getElementsByName('Ans834')[2].value='迁移作品经验';
+document.getElementsByName('Ans834')[3].value='创造性想象和语言表达';
+//一、2
+document.getElementsByName('Ans835')[0].value='民间故事';
+document.getElementsByName('Ans835')[1].value='创编故事';
+document.getElementsByName('Ans835')[2].value='幼儿园图画故事';
+document.getElementsByName('Ans835')[3].value='生活故事';
+document.getElementsByName('Ans835')[4].value='人物故事';
+document.getElementsByName('Ans835')[5].value='历史故事';
+document.getElementsByName('Ans835')[6].value='通话故事';
+document.getElementsByName('Ans835')[7].value='动物故事';
+document.getElementsByName('Ans835')[8].value='科学故事';
+//一、3
+document.getElementsByName('Ans836')[0].value='全文复述';
+document.getElementsByName('Ans836')[1].value='细节复述';
+//一、4
+document.getElementsByName('Ans837')[0].value='复述';
+
+//二、1
+document.getElementById('Ans40').click();
+document.getElementById('Ans41').click();
+document.getElementById('Ans42').click();
+document.getElementById('Ans43').click();
+//二、2
+document.getElementById('Ans50').click();
+document.getElementById('Ans51').click();
+document.getElementById('Ans53').click();
+//二、3
+document.getElementById('Ans60').click();
+document.getElementById('Ans61').click();
+document.getElementById('Ans62').click();
+
+//三、1
+document.getElementById('Ans71').click();
+//三、2
+document.getElementById('Ans80').click();
+//三、3
+document.getElementById('Ans91').click();
+//三、4
+document.getElementById('Ans100').click();
+//三、5
+document.getElementById('Ans110').click();
+
+//四、1
+document.getElementById('Ans12').innerHTML=
+'    第一层次：学习文学作品\n'+
+'    学习故事《会唱歌的生日蛋糕》时，教师不仅让幼儿讨论：故事里的小熊遇到了一件什么样的事？他们是怎样做的？你喜欢小熊吗？为什么？而且还引导幼儿谈论：如果你是故事里的小熊，你怎样祝贺外婆生日？如果你有一个会唱歌的生日蛋糕，你用它来做什么？这些问题都有助于加深幼儿对故事的理解。\n'+
+'    第二层次：理解体验作品\n'+
+'    《会唱歌的生日蛋糕》作品教学之后，教师组织幼儿学唱生日歌，又在生日歌乐曲的伴随声中开展故事表演游戏。这些活动都是围绕生日开展的，因为《会唱歌的生日蛋糕》的主要情节、人物角色均与生日有关。通过学唱生日歌和故事表演游戏，幼儿进一步设身处地感受了生日气氛，体验了故事中小动物们准备过生日的情绪。特别是故事表演游戏，用玩具材料和游戏活动创造出一个假想的天地，允许幼儿在玩的时候再现作品的想象，经历小熊和其他动物的活动过程，能较好地帮助幼儿体验作品中欢乐的情绪，加深对作品的理解。\n'+
+'    第三层次：迁移作品经验\n'+
+'    在故事《会唱歌的生日蛋糕》中，教师可以让幼儿做一做生日礼物，再给本班小朋友开个庆贺生日会，像童话中的小动物那样为别人的生日做点使他们高兴的事。\n'+
+'    第四层次：创造性想象和语言表述\n'+
+'    在《会唱歌的生日蛋糕》活动过程中，教师在最后一个层次可以以“你想怎样给爸爸妈妈过生日”为题，组织幼儿讨论、讲述。这样，每个幼儿都可能愿意像故事中的小熊那样，为自己的爸爸妈妈过个快乐的生日，并充分地开动脑筋、大胆表达，想出许多有趣、也很有意义的祝福方式，使活动在幼儿欢快、热烈的交流中结束。';
+
+//四、2
+document.getElementById('Ans13').innerHTML=
+'帮助幼儿记忆的提问；针对细节的提问；针对情感识别与匹配的提问；针对故事作品的主题或情节的提问；针对作品中文学语言的提问；针对作品'+'整体结构形式的提问；针对生活原型与作品形象进行比较的提问。';
