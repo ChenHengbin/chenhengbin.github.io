2274c0af785745534a72d369a1e95e3990b91041
ChenHengbin <812682293@qq.com>
2014-06-03 09:58:32 +0800

    1
    1

---
+++ js/homework/450.js
@@ -0,0 +1,41 @@
+﻿//一、1
+document.getElementsByName('Ans687')[0].value='门厅';
+document.getElementsByName('Ans687')[1].value='走廊';
+document.getElementsByName('Ans687')[2].value='楼梯';
+document.getElementsByName('Ans687')[3].value='交通功能';
+document.getElementsByName('Ans687')[4].value='教育功能';
+//一、2
+document.getElementsByName('Ans688')[0].value='绿化功能';
+document.getElementsByName('Ans688')[1].value='展示功能';
+document.getElementsByName('Ans688')[2].value='宣传功能';
+//一、3
+document.getElementsByName('Ans689')[0].value='教师用以引发幼儿学习';
+document.getElementsByName('Ans689')[1].value='主体性';
+
+//二、1
+document.getElementById('Ans31').click();
+//二、2
+document.getElementById('Ans43').click();
+
+//三、1
+document.getElementById('Ans50').click();
+//三、2
+document.getElementById('Ans60').click();
+//三、3
+document.getElementById('Ans70').click();
+
+//四、1
+document.getElementsByName('Ans695')[0].value=
+'室内装饰的策略：\n'+
+'（一）持久性布置策略\n'+
+'1．门厅\n'+
+'首先是确定门厅要给予来访者留下一个什么样的印象，是冷硬的还是柔和的，是大众型的还是突显特色的。再根据门厅的空间大小进行具体的设计。如果幼儿园的门厅空间较大，装饰设计初期可以利用门厅现有界面，巧妙装饰不同形状、不同面积的壁画，体现独有的空间的文化内涵。壁画部分正好掩饰其他部位的不协调的建筑，拐角墙之类可以装饰食谱系列挂板，展示幼儿食谱和活动剪影，凸现活泼、有趣和实用的效果。\n'+
+'2．走廊\n'+
+'如果走廊比较宽，在走廊里增设储物柜可以起到衣帽间的作用。开放式的走廊与户外空间直接相连，如果宽度在2米以上，除满足交通外，走廊还可作为阴雨天气里体育锻炼的场所。走廊也是展示幼儿作品的好地方。封闭式的内走廊，可以对其护栏、廊柱、地面、天花板进行整体设计。每个班级门前的走廊以设置3-4个区域为宜，每个区域大约容纳4-5人。\n'+
+'3．楼梯\n'+
+'楼梯的潜在功能很多，如绿化功能、展示功能、宣传功能。 \n'+
+'（二）动态性布置策略\n'+
+'1. 与季节变换的融合\n'+
+'自然界的四季，每一季均有一个基本色调：春季的浅绿色、夏季的深绿色、秋季的金黄色、冬季的雪白色。除了色彩，每一季还有自己独特的符号——不同季节中出现的常见动物或昆虫。季节的声、色、形，可以通过公共空间环境的布置，使幼儿和家长感到如同置身自然中，而倍感亲切与放松。\n'+
+'2. 与节日或主题教育的融合\n'+
+'将节日引入环境布置时，要注意不可过多、过泛。此外主题课程也常常会选用节日作为主题。当主题活动与节日重叠时，室内装饰宜做全园整体性的设计。';
