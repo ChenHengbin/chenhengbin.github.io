0e363479e95d30b9fba5c8c03ae13b7cd5362967
ChenHengbin <812682293@qq.com>
2014-06-10 11:05:11 +0800

    1
    1

---
+++ js/homework/530.js
@@ -0,0 +1,35 @@
+﻿//一、1
+document.getElementsByName('Ans1272')[0].value='情绪健康';
+document.getElementsByName('Ans1272')[1].value='行为习惯';
+//一、2
+document.getElementsByName('Ans1281')[0].value='较高';
+//一、3
+document.getElementsByName('Ans1282')[0].value='宣泄情绪';
+//一、4
+document.getElementsByName('Ans1283')[0].value='口吃';
+document.getElementsByName('Ans1283')[1].value='自慰';
+
+//二、1
+document.getElementById('Ans41').click();
+//二、2
+document.getElementById('Ans53').click();
+
+//三、1
+document.getElementById('Ans60').click();
+//三、2
+document.getElementById('Ans71').click();
+//三、3
+document.getElementById('Ans81').click();
+//三、4
+document.getElementById('Ans90').click();
+
+//四、1
+document.getElementById('Ans10').value = 
+'（1）耐心；\n'+
+'（2）了解幼儿需求是否得到满足；\n'+
+'（3）养成良好卫生习惯；\n'+
+'（4）及时制止并分散幼儿注意力。';
+
+//四、2
+document.getElementById('Ans11').value = 
+'（1）教师必须剧本健康的心理素质；（2）掌握儿童心理特点；（3）营造健康的氛围；（4）正确对待个性差异。';
---
+++ js/homework/532.js
@@ -0,0 +1,24 @@
+﻿//一、1
+document.getElementsByName('Ans1297')[0].value='身体素质';
+//一、2
+document.getElementsByName('Ans1298')[0].value='徒手操';
+
+//二、1
+document.getElementById('Ans23').click();
+//二、2
+document.getElementById('Ans33').click();
+
+//三、1
+document.getElementById('Ans40').click();
+//三、2
+document.getElementById('Ans51').click();
+//三、3
+document.getElementById('Ans61').click();
+
+
+//四、1
+document.getElementById('Ans7').value = 
+'（1）坚持“健康第一”的指导思想；\n'+
+'（2）注重激发与保持幼儿活动兴趣；\n'+
+'（3）关注个体差异和不同需求；\n'+
+'（4）选用适当的背景音乐。';
