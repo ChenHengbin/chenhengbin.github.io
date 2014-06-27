28dc645bb188976689a9234fed54c57918c6c498
ChenHengbin <812682293@qq.com>
2014-06-27 21:01:20 +0800

    1
    1

---
+++ js/homework/435.js
@@ -0,0 +1,26 @@
+﻿//一、1
+document.getElementsByName('Ans391')[0].value='个数';
+document.getElementsByName('Ans391')[1].value='排列次序';
+//一、2
+document.getElementsByName('Ans392')[0].value='计数 活动';
+//一、3
+document.getElementsByName('Ans393')[0].value='认识10以内数的实际意义';
+document.getElementsByName('Ans393')[1].value='认识10以内相邻数的关系';
+document.getElementsByName('Ans393')[2].value='掌握10以内数的组成';
+
+//二、1
+document.getElementById('Ans30').click();
+//二、2
+document.getElementById('Ans42').click();
+
+//三、1
+document.getElementById('Ans51').click();
+//三、2
+document.getElementById('Ans60').click();
+
+//四、1
+document.getElementsByName('Ans398')[0].value=
+'（1）教幼儿通过观察和比较初步认识“1”和“许多”，区分哪些物体是1个的，哪些物体是许多的。\n'+
+'（2）教幼儿运用各种感官感知“1”和“许多”。\n'+
+'（3）教幼儿通过分与合的操作活动，初步理解“1”和“许多”的关系，知道“许多”可以分成“1、1、1…”，“1、1、1…”合起来是“许多”。\n'+
+'（4）通过寻找物体的活动，加深幼儿对“1”和“许多”的认识。寻找的方式有：①让幼儿在已准备好的环境中找一个物体和许多个物体；②引导幼儿在自然环境中寻找“1”和“许多”；③引导幼儿用记忆表象寻找“1”和“许多”。';
