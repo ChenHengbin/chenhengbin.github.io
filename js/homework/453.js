37fa2d2560d92f541955fd7721cd996b750abd7d
ChenHengbin <812682293@qq.com>
2014-06-29 16:38:55 +0800

    1
    1

---
+++ js/homework/453.js
@@ -0,0 +1,42 @@
+﻿//一、1
+document.getElementsByName('Ans713')[0].value='游戏';
+//一、2
+document.getElementsByName('Ans714')[0].value='感觉运动游戏';
+document.getElementsByName('Ans714')[1].value='象征性游戏';
+document.getElementsByName('Ans714')[2].value='规则游戏';
+//一、3
+document.getElementsByName('Ans715')[0].value='角色游戏';
+document.getElementsByName('Ans715')[1].value='结构游戏';
+document.getElementsByName('Ans715')[2].value='表演游戏';
+
+//二、1
+document.getElementById('Ans32').click();
+//二、2
+document.getElementById('Ans43').click();
+
+//三、1
+document.getElementById('Ans51').click();
+//三、2
+document.getElementById('Ans61').click();
+//三、3
+document.getElementById('Ans70').click();
+
+//四、1
+document.getElementsByName('Ans721')[0].value=
+'1、幼儿游戏的作用包括：\n'+
+'（1）游戏在幼儿身体发展中的作用 ：促进幼儿跑跳，躲闪的行为能力。\n'+
+'（2）游戏在幼儿认知发展中的作用。\n'+
+'A/.游戏扩展和加深幼儿对周围事物的认识。\n'+
+'B/游戏促进幼儿想象力的发展。\n'+
+'C游戏发展幼儿的思维能力。\n'+
+'D/游戏促进幼儿语言的发展。\n'+
+'E/游戏提供了幼儿智力活动的轻松愉悦的心理氛围。\n'+
+'（3）游戏在幼儿社会性发展中的作用。\n'+
+'1. 游戏为幼儿提供了社会交往的机会，发展了幼儿的社会交往能力。\n'+
+'2. 游戏使幼儿学习社会角色，掌握社会性行为规范。\n'+
+'3. 游戏锻炼了幼儿的意志。\n'+
+'4. 游戏有助于幼儿克服自我中心化，学会理解他人。\n'+
+'（4）游戏在幼儿情绪情感发展中的作用。\n'+
+'1. 游戏可以丰富幼儿的情绪体验。\n'+
+'2. 游戏发展幼儿的成就感和美感。\n'+
+'3. 游戏可以消除幼儿的消极情绪。';
---
+++ js/homework/460.js
@@ -0,0 +1,24 @@
+﻿//一、1
+document.getElementsByName('Ans779')[0].value='交际礼仪';
+//一、2
+document.getElementsByName('Ans780')[0].value='适度原则';
+document.getElementsByName('Ans780')[1].value='自律原则 ';
+//一、3
+document.getElementsByName('Ans781')[0].value='坐姿';
+
+//二、1
+document.getElementById('Ans33').click();
+//二、2
+document.getElementById('Ans40').click();
+
+//三、1
+document.getElementById('Ans50').click();
+//三、2
+document.getElementById('Ans60').click();
+//三、3
+document.getElementById('Ans71').click();
+
+//四、1
+document.getElementsByName('Ans787')[0].value=
+'1、“尊重”是老师与家长沟通的前提\n'+
+'2、充分发挥语言艺术的魅力 注意语言的形式和方式 把握好沟通的时序多给家长一些建设性的意见，根据不同的家长选择不同的表达方式，要重视非言语提示的重要作用。多给予赞赏和鼓励，';
